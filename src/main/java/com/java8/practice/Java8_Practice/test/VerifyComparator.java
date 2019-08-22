58148220ef0a4d7a9bb3b707d852800ad8e4f80d
kprasannanjaneyulu <kprasannanjaneyulu@KPRASANNANJANE.pkrmsezdc.com>
2019-08-22 10:46:26 -0500

    Comparator thenComparing() example

--- src/main/java/com/java8/practice/Java8_Practice/test/VerifyComparator.java
+++ src/main/java/com/java8/practice/Java8_Practice/test/VerifyComparator.java
@@ -10,19 +10,23 @@ import com.java8.practice.Java8_Practice.vo.Employee;
 public class VerifyComparator {
 	
 	public static void main(String[] args) {
-		Comparator<Employee> compareById = (Employee o1, Employee o2) -> o1.getId().compareTo(o2.getId());
-		List<Employee> employees = getEmployees().stream().sorted(compareById).collect(Collectors.toList());
+		Comparator<Employee> compareByAge = (Employee o1, Employee o2) -> o1.getAge().compareTo(o2.getAge());
+		Comparator<Employee> compareByFirstName = (Employee o1, Employee o2) -> o1.getFirstName().compareTo(o2.getFirstName());
+		Comparator<Employee> compareByLastName = (Employee o1, Employee o2) -> o1.getLastName().compareTo(o2.getLastName());
+		Comparator<Employee> compareCompositeFields=compareByAge.thenComparing(compareByFirstName).thenComparing(compareByLastName);
+		List<Employee> employees = getEmployees().stream().sorted(compareCompositeFields).collect(Collectors.toList());
 		System.out.println(employees);
 	}
 
 	private static List<Employee> getEmployees() {
 		List<Employee> employees = Lists.newArrayList();
-		employees.add(new Employee(6, "Yash", "Chopra", 25));
+		employees.add(new Employee(6, "Yash", "Chopra", 60));
 		employees.add(new Employee(2, "Aman", "Sharma", 28));
 		employees.add(new Employee(3, "Aakash", "Yaadav", 52));
 		employees.add(new Employee(5, "David", "Kameron", 19));
 		employees.add(new Employee(4, "James", "Hedge", 72));
 		employees.add(new Employee(8, "Balaji", "Subbu", 88));
+		employees.add(new Employee(11, "Balaji", "Dev", 88));
 		employees.add(new Employee(7, "Karan", "Johar", 59));
 		employees.add(new Employee(1, "Lokesh", "Gupta", 32));
 		employees.add(new Employee(9, "Vishu", "Bissi", 33));
