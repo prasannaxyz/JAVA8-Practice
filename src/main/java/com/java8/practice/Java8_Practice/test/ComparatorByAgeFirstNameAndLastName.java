0a5c7d183276949224fafbd80b9f79cbdb456f49
kprasannanjaneyulu <kprasannanjaneyulu@KPRASANNANJANE.pkrmsezdc.com>
2019-08-22 23:43:47 -0500

    Max and min comparator

--- src/main/java/com/java8/practice/Java8_Practice/test/ComparatorByAgeFirstNameAndLastName.java
+++ src/main/java/com/java8/practice/Java8_Practice/test/ComparatorByAgeFirstNameAndLastName.java
@@ -2,21 +2,41 @@ package com.java8.practice.Java8_Practice.test;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
 
 import com.google.common.collect.Lists;
 import com.java8.practice.Java8_Practice.vo.Employee;
 
-/*Comparator thenComparing() example*/
+/*Comparator thenComparing(), Calculating max and min examples*/
 public class ComparatorByAgeFirstNameAndLastName {
-	
+
 	public static void main(String[] args) {
 		Comparator<Employee> compareByAge = (Employee o1, Employee o2) -> o1.getAge().compareTo(o2.getAge());
-		Comparator<Employee> compareByFirstName = (Employee o1, Employee o2) -> o1.getFirstName().compareTo(o2.getFirstName());
-		Comparator<Employee> compareByLastName = (Employee o1, Employee o2) -> o1.getLastName().compareTo(o2.getLastName());
-		Comparator<Employee> compareCompositeFields=compareByAge.thenComparing(compareByFirstName).thenComparing(compareByLastName);
+		Comparator<Employee> compareByFirstName = (Employee o1, Employee o2) -> o1.getFirstName()
+				.compareTo(o2.getFirstName());
+		Comparator<Employee> compareByLastName = (Employee o1, Employee o2) -> o1.getLastName()
+				.compareTo(o2.getLastName());
+		Comparator<Employee> compareCompositeFields = compareByAge.thenComparing(compareByFirstName)
+				.thenComparing(compareByLastName);
 		List<Employee> employees = getEmployees().stream().sorted(compareCompositeFields).collect(Collectors.toList());
 		System.out.println(employees);
+		if (CollectionUtils.isNotEmpty(employees)) {
+			Comparator<Employee> comparatorByAge = Comparator.comparing(Employee::getAge);
+			Optional<Employee> minAgeEmployee = employees.stream().min(comparatorByAge);
+			Optional<Employee> maxAgeEmployee = employees.stream().max(comparatorByAge);
+
+			if (minAgeEmployee.isPresent()) {
+				System.out.println(
+						"minAge = " + minAgeEmployee.get().getFirstName() + " " + minAgeEmployee.get().getAge());
+			}
+			if (maxAgeEmployee.isPresent()) {
+				System.out.println(
+						"maxAge = " + maxAgeEmployee.get().getFirstName() + " " + maxAgeEmployee.get().getAge());
+			}
+		}
 	}
 
 	private static List<Employee> getEmployees() {
